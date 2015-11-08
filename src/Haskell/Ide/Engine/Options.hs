ade80331283f2c156b7ff5ae495b328a8d4a08a1
Michael Sloan <mgsloan@gmail.com>
2015-11-08 03:13:42 -0800

    Remove some old commented out code

--- src/Haskell/Ide/Engine/Options.hs
+++ src/Haskell/Ide/Engine/Options.hs
@@ -27,22 +27,3 @@ globalOptsParser = GlobalOpts
         <> metavar "LOGFILE"
         <> help "File to log to, defaults to stdout"
          ))
-{-
-data GlobalOpts = GlobalOpts
-    { optPluginModules :: [String]
-    , optPluginPackages :: [String]
-    } deriving (Show)
-
-globalOptsParser :: Parser GlobalOpts
-globalOptsParser = GlobalOpts
-    <$> many (strOption
-        ( long "plugin"
-       <> metavar "MODULE"
-       <> help "Specify a plugin to load"
-        ))
-    <*>  many (strOption
-        ( long "plugin-pkg"
-       <> metavar "PACKAGE"
-       <> help "Specify a package to use for loading plugins"
-        ))
--}
