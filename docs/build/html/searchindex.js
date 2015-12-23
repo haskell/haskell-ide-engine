c230775e89f55b71358d37d59eae1f811c0a67d0
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-12-23 20:42:06 +0100

    Initial documentation from sphinx-quickstart

---
+++ docs/build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({envversion:47,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to haskell-ide-engine&#8217;s documentation!"],titleterms:{document:0,engin:0,haskel:0,indic:0,tabl:0,welcom:0}})
