683c48c9046d078483d8e7b7e3f1c81d2965bb21
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2015-12-28 21:53:10 +0100

    Show git output

--- deploy-docs.sh
+++ deploy-docs.sh
@@ -34,4 +34,4 @@ git commit -m "Deploy to GitHub Pages"
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
