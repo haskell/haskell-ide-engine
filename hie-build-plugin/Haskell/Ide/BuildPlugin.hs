3f1a6403c37ef995486ffc327368dfb483618f26
Gleb Popov <6yearold@gmail.com>
2016-01-13 20:07:55 +0300

    Attach build-plugin to the build.

---
+++ hie-build-plugin/Haskell/Ide/BuildPlugin.hs
@@ -0,0 +1,137 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
+module Haskell.Ide.BuildPlugin where
+
+import           Control.Concurrent
+import           Control.Concurrent.STM.TChan
+import           Control.Monad.IO.Class
+import           Control.Monad.STM
+import           Haskell.Ide.Engine.ExtensibleState
+import           Haskell.Ide.Engine.MonadFunctions
+import           Haskell.Ide.Engine.PluginDescriptor
+import           Haskell.Ide.Engine.PluginUtils
+import           Data.Monoid
+import qualified Data.Map as Map
+import qualified Data.Text as T
+import System.IO.Error
+
+import Distribution.PackageDescription
+import Distribution.PackageDescription.Parse
+import qualified Distribution.Verbosity as Verb
+
+-- ---------------------------------------------------------------------
+
+exampleAsyncDescriptor :: PluginDescriptor
+exampleAsyncDescriptor = PluginDescriptor
+  {
+    pdUIShortName = "Build plugin"
+  , pdUIOverview = "A HIE plugin for building cabal/stack packages"
+  , pdCommands =
+      [
+        buildCommand (longRunningCmdSync Cmd1) "cmd1" "Long running synchronous command" [] [CtxNone] []
+      , buildCommand addTarget "addTarget" "Add a new target to the cabal file" [] [CtxNone]
+                               [ RP "file" "Path to the .cabal file" PtFile
+                               , RP "name" "Name of the new target" PtText
+                               , RP "type" "executable/library" PtText
+                               ]
+      , buildCommand (longRunningCmdSync Cmd2) "cmd2" "Long running synchronous command" [] [CtxNone] []
+      ]
+  , pdExposedServices = []
+  , pdUsedServices    = []
+  }
+
+-- ---------------------------------------------------------------------
+
+data WorkerCmd = Cmd1 | Cmd2
+               deriving Show
+
+-- | Keep track of the communication channesl to the remote process.
+data SubProcess = SubProcess
+  { spChIn    :: TChan WorkerCmd
+  , spChOut   :: TChan T.Text
+  , spProcess :: ThreadId
+  }
+
+-- | Wrap it in a Maybe for pure initialisation
+data AsyncPluginState = APS (Maybe SubProcess)
+
+-- | Tag the state variable to enable it to be stored in the dispatcher state,
+-- accessible to all plugins, provided they know the type, as it is accessed via
+-- a @cast@
+instance ExtensionClass AsyncPluginState where
+  initialValue = APS Nothing
+
+-- ---------------------------------------------------------------------
+
+addTarget = CmdSync $ \ctx req -> do
+    let args = (,,) <$> Map.lookup "file" (ideParams req)
+                    <*> Map.lookup "name" (ideParams req)
+                    <*> Map.lookup "type" (ideParams req)
+    case args of
+        Nothing -> return $ missingParameter "file, name or type"
+        Just (ParamFileP file, ParamTextP name, ParamTextP _type) -> do
+            res <- liftIO $ addTarget' (T.unpack file) (T.unpack name) (T.unpack _type)
+            return $ case res of
+                False -> missingParameter "meh"
+                True -> IdeResponseOk ()
+
+addTarget' :: FilePath -> String -> String -> IO Bool
+addTarget' file name _type = do
+    parseRes <- parsePackageDescription <$> readFile file
+    case parseRes of
+        ParseFailed _ -> return False
+        ParseOk _ genPackDesc -> do
+            let newDescr = case _type of
+                    "executable" -> genPackDesc {
+                        condExecutables = condExecutables genPackDesc <>
+                            [(name, CondNode emptyExecutable [] [])]
+                        }
+            --writePackageDescription file newDescr
+            return True
+
+-- | This command manages interaction with a separate process, doing stuff.
+longRunningCmdSync :: WorkerCmd -> CommandFunc T.Text
+longRunningCmdSync cmd = CmdSync $ \_ctx req -> do
+  SubProcess cin cout _tid <- ensureProcessRunning
+  liftIO $ atomically $ writeTChan cin cmd
+  res <- liftIO $ atomically $ readTChan cout
+  return (IdeResponseOk $ "res=" <> res)
+
+-- ---------------------------------------------------------------------
+
+-- | If there is already a @SubProcess@ value in the plugin state return it,
+-- else create a new set of @TChan@ and fork the worker with them, storing the
+-- new @SubProcess@ value in the plugin state.
+ensureProcessRunning :: IdeM SubProcess
+ensureProcessRunning = do
+  (APS v) <- get -- from extensible state
+  sp <- case v of
+    Nothing -> do
+      cin  <- liftIO $ atomically newTChan
+      cout <- liftIO $ atomically newTChan
+      tid  <- liftIO $ forkIO (workerProc cin cout)
+      let v' = SubProcess cin cout tid
+      put (APS (Just v')) -- into extensible state
+      return v'
+    Just v' -> return v'
+  return sp
+
+-- ---------------------------------------------------------------------
+
+-- | Long running worker process, can be doing commands in an async manner
+workerProc :: TChan WorkerCmd -> TChan T.Text -> IO ()
+workerProc cin cout = loop 1
+  where
+    loop cnt = do
+      debugm "workerProc:top of loop"
+      req <- liftIO $ atomically $ readTChan cin
+      debugm $ "workerProc loop:got:" ++ show req
+      case req of
+        Cmd1 -> do
+          liftIO $ atomically $ writeTChan cout (T.pack $ "wp cmd1:cnt=" ++ show cnt)
+          loop (cnt + 1)
+        Cmd2 -> do
+          liftIO $ atomically $ writeTChan cout (T.pack $ "wp cmd2:cnt=" ++ show cnt)
+          loop (cnt + 1)
+
+-- ---------------------------------------------------------------------
