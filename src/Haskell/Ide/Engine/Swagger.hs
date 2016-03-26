41a03cf785a49fa146829407ebdfbd4ccf60a2ae
Alan Zimmerman <alan.zimm@gmail.com>
2016-03-26 21:27:31 +0200

    Required params now encoded

--- src/Haskell/Ide/Engine/Swagger.hs
+++ src/Haskell/Ide/Engine/Swagger.hs
@@ -11,9 +11,6 @@ module Haskell.Ide.Engine.Swagger
     hieSwagger
   ) where
 
--- Based on https://github.com/GetShopTV/swagger2/blob/master/examples/hackage.hs
--- TODO: Make sure we comply with license conditions.
-
 import           Control.Lens
 import           Data.Aeson
 import           Data.List
@@ -80,220 +77,51 @@ commandToPath pName c@(Command cd f) = do
 
 -- ---------------------------------------------------------------------
 
--- TODO: required params, use a ref for the standard types
 mkParamsSchema :: Referenced Schema -> Referenced Schema -> [ParamDescription] -> Schema
 mkParamsSchema fileSchemaRef textSchemaRef allParams = s
   where
     pList = map mkParam allParams
 
+    requiredParams = concatMap isRequired allParams
+
+    isRequired :: ParamDescription -> [Data.Swagger.ParamName]
+    isRequired pd = if pRequired pd == Required
+                       then [pName pd]
+                       else []
+
     mkParam pd = (pName pd,pTypeSchema (pType pd))
 
-    -- pTypeSchema PtText = toSchemaRef (Proxy :: Proxy T.Text)
     pTypeSchema PtText = textSchemaRef
     pTypeSchema PtFile = fileSchemaRef
     pTypeSchema PtPos  = toSchemaRef (Proxy :: Proxy Pos)
+      -- For some reason the Pos reference target does end up in the swagger
+      -- definition, so does not need to be passed in
 
     s = mempty
       & type_ .~ SwaggerObject
+      & required .~ requiredParams
       & properties .~ (HM.fromList pList)
 
+-- ---------------------------------------------------------------------
+
+-- | local type declaration for swagger, so that the generated Schema matches
+-- what HIE is expecting
 data File = File { file::T.Text} deriving Generic
 instance ToSchema File
 
+-- | local type declaration for swagger, so that the generated Schema matches
+-- what HIE is expecting
 data Text = Text { text::T.Text} deriving Generic
 instance ToSchema Text
 
 -- ---------------------------------------------------------------------
 
-{-
-{
-  "type": "object",
-  "required": [
-    "name"
-  ],
-  "properties": {
-    "name": {
-      "type": "string"
-    },
-    "address": {
-      "$ref": "#/definitions/Address"
-    },
-    "age": {
-      "type": "integer",
-      "format": "int32",
-      "minimum": 0
-    }
-  }
-}
--}
-
--- ---------------------------------------------------------------------
-
-swaggerParamSchema :: ParamDescription -> Declare (Definitions Schema) (Referenced Schema)
-swaggerParamSchema pd = do
-
-  textSchema <- declareSchema (Proxy :: Proxy T.Text)
-  posSchema  <- declareSchema (Proxy :: Proxy Pos)
-
-  let
-    pSchema PtText = textSchema
-    pSchema PtFile = textSchema
-    pSchema PtPos  = posSchema
-
-  let req Required = True
-      req Optional = False
-  let p = Inline $ pSchema (pType pd)
-  return p
-
--- ---------------------------------------------------------------------
-
-swaggerParam :: ParamDescription -> Declare (Definitions Schema) (Referenced Param)
-swaggerParam pd = do
-
-  textSchema <- declareSchema (Proxy :: Proxy T.Text)
-  posSchema  <- declareSchema (Proxy :: Proxy Pos)
-
-  let
-    pSchema PtText = textSchema
-    pSchema PtFile = textSchema
-    pSchema PtPos  = posSchema
-
-  let req Required = True
-      req Optional = False
-  let p = Inline $ mempty
-                 & name .~ pName pd
-                 & required ?~ req (pRequired pd)
-                 & schema .~ ParamBody
-                      ( Inline $ pSchema (pType pd)
-                      )
-  return p
-
--- pSchema :: ParamType -> Schema
--- pSchema PtText = toParamSchema (Proxy :: Proxy T.Text)
--- pSchema PtFile = toParamSchema (Proxy :: Proxy T.Text)
--- pSchema PtPos  = toParamSchema (Proxy :: Proxy Pos)
-
--- ---------------------------------------------------------------------
-
--- instance {-# OVERLAPPING #-} (ToSchema a,ToSchema b) => ToParamSchema (a,b) where
---   toParamSchema proxy = mempty
---     & items ?~ SwaggerItemsArray [Inline $ toSchema (Proxy :: Proxy a),
---                                   Inline $ toSchema (Proxy :: Proxy b)]
---     -- & items ?~ SwaggerItemsArray [Inline $ toSchema (Proxy :: Proxy a),
---     --                               Inline $ toSchema (Proxy :: Proxy b)]
---     & type_ .~ SwaggerArray
-
--- ---------------------------------------------------------------------
-
--- instance ToParamSchema (Int,Int)
--- instance ToSchema (Int,Int) where
---   toSchema proxy = (mempty
---     & type_ .~ SwaggerArray)
---     -- & items ?~ SwaggerItemsArray []
---     -- & items ?~ SwaggerItemsPrimitive Nothing (toParamSchema (Proxy :: Proxy Int))
-
---      -- { _paramSchemaItems = Just (Ref foo)
---      -- }
---      -- { _paramSchemaItems = Just (SwaggerItemsArray [Inline (toSchema (Proxy :: Proxy Int)),
---      --                                                Inline (toSchema (Proxy :: Proxy Int))])
---      -- }
---     & items ?~ SwaggerItemsArray [Inline (toSchema (Proxy :: Proxy Int)),
---                                   Inline (toSchema (Proxy :: Proxy Int))]
-
-{-
-  "parameters": [
-      {
-          "required": true,
-          "schema": {
-              "items": [
-                  {
-                      "maximum": 9223372036854775807,
-                      "minimum": -9223372036854775808,
-                      "type": "integer"
-                  },
-                  {
-                      "maximum": 9223372036854775807,
-                      "minimum": -9223372036854775808,
-                      "type": "integer"
-                  }
-              ],
-              "type": "array"
-          },
-          "in": "body",
-          "name": "body"
-      }
-
--}
-
-
--- ---------------------------------------------------------------------
-
+-- Keep the type checker happy while extracting the reply schema
 commandResponse :: UntaggedCommand -> Declare (Definitions Schema) Response
 commandResponse (Command x f) = declareCmdResponse f
-
--- ---------------------------------------------------------------------
-
--- declareResponse :: ToSchema a => proxy a -> Declare (Definitions Schema) Response
 
 declareCmdResponse :: ToSchema a => CommandFunc a -> Declare (Definitions Schema) Response
 declareCmdResponse = declareResponse
 
 -- ---------------------------------------------------------------------
 
-{-
-
-type Username = Text
-
-data UserSummary = UserSummary
-  { summaryUsername :: Username
-  , summaryUserid   :: Int
-  } deriving (Generic, ToSchema)
-
-type Group = Text
-
-data UserDetailed = UserDetailed
-  { username :: Username
-  , userid   :: Int
-  , groups   :: [Group]
-  } deriving (Generic, ToSchema)
-
-newtype Package = Package { packageName :: Text }
-  deriving (Generic, ToSchema)
-
-hackageSwagger :: Swagger
-hackageSwagger = spec & definitions .~ defs
-  where
-    (defs, spec) = runDeclare declareHackageSwagger mempty
-
-declareHackageSwagger :: Declare (Definitions Schema) Swagger
-declareHackageSwagger = do
-  -- param schemas
-  let usernameParamSchema = toParamSchema (Proxy :: Proxy Username)
-
-  -- responses
-  userSummaryResponse   <- declareResponse (Proxy :: Proxy UserSummary)
-  userDetailedResponse  <- declareResponse (Proxy :: Proxy UserDetailed)
-  packagesResponse      <- declareResponse (Proxy :: Proxy [Package])
-
-
--- DO NOT EDIT HERE
-  return $ mempty
-    & paths .~
-        [ ("/users", mempty & get ?~ (mempty
-            & produces ?~ MimeList ["application/json"]
-            & at 200 ?~ Inline userSummaryResponse))
-        , ("/user/{username}", mempty & get ?~ (mempty
-            & produces ?~ MimeList ["application/json"]
-            & parameters .~ [ Inline $ mempty
-                & name .~ "username"
-                & required ?~ True
-                & schema .~ ParamOther (mempty
-                    & in_ .~ ParamPath
-                    & paramSchema .~ usernameParamSchema) ]
-            & at 200 ?~ Inline userDetailedResponse))
--- DO NOT EDIT HERE
-        , ("/packages", mempty & get ?~ (mempty
-            & produces ?~ MimeList ["application/json"]
-            & at 200 ?~ Inline packagesResponse))
-        ]
--}
