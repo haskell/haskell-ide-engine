5d9d7d5cf69ea2063013f197dd75de80065552eb
Alan Zimmerman <alan.zimm@gmail.com>
2016-09-05 11:17:34 +0200

    Only get the ghc trees for the selected file

--- hie-ghc-tree/Haskell/Ide/GhcTreePlugin.hs
+++ hie-ghc-tree/Haskell/Ide/GhcTreePlugin.hs
@@ -11,11 +11,14 @@ module Haskell.Ide.GhcTreePlugin where
 
 import           Data.Aeson
 import qualified Data.Text as T
+-- import           Haskell.Ide.Engine.MonadFunctions (logm)
 import           Haskell.Ide.Engine.PluginDescriptor
 import           Haskell.Ide.Engine.PluginUtils
 import           Haskell.Ide.Engine.SemanticTypes
 import           Language.Haskell.GHC.DumpTree
 import           Language.Haskell.GhcMod.Monad
+
+-- ---------------------------------------------------------------------
 
 -- | Descriptor for the ghc-tree plugin
 ghcTreeDescriptor :: TaggedPluginDescriptor _
@@ -41,7 +44,7 @@ trees = CmdSync $ \_ctxs req -> do
   case getParams (IdFile "file" :& RNil) req of
     Left err -> return err
     Right (ParamFile fileName :& RNil) -> do
-      trs <- runGmlT' [Left $ T.unpack fileName] (return . treeDumpFlags)$ treesForSession
+      trs <- runGmlT' [Left $ T.unpack fileName] (return . treeDumpFlags) $ treesForTargets [T.unpack fileName]
       -- logm $ "getTrees:res=" ++ show trs
       case trs of
           [tree] -> return (IdeResponseOk $ treesToAST tree)
