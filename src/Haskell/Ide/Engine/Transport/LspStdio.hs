013af19aab552466dea4c841866f8d38e3b1bc58
Alan Zimmerman <alan.zimm@gmail.com>
2016-10-17 23:11:43 +0200

    Bring in reactor process to share bridging state...
    between the hie dispatcher and the haskell-lsp callback handler.

--- src/Haskell/Ide/Engine/Transport/LspStdio.hs
+++ src/Haskell/Ide/Engine/Transport/LspStdio.hs
@@ -17,6 +17,7 @@ import           Control.Monad
 import           Control.Monad.IO.Class
 import           Control.Monad.STM
 import qualified Data.Aeson as J
+import qualified Data.ByteString.Lazy as BSL
 import           Data.Default
 import           Data.Either.Utils
 import qualified Data.Map as Map
@@ -34,6 +35,7 @@ import qualified Pipes.Prelude as P
 import           System.Exit
 import           System.IO
 import qualified System.Log.Logger as L
+
 
 -- ---------------------------------------------------------------------
 {-# ANN module ("hlint: ignore Eta reduce" :: String) #-}
@@ -54,15 +56,15 @@ run :: TChan ChannelRequest -> IO Int
 run cin = flip E.catches handlers $ do
 
   cout <- atomically newTChan :: IO (TChan ChannelResponse)
+  rin  <- atomically newTChan :: IO (TChan ReactorInput)
   U.logs $ "\n\n*************************about to fork responseHandler"
-  _ <- forkIO $ responseHandler cout
-  U.logs $ "\n\n*************************forked responseHandler"
-  -- Give responseHandler a chance to start
-  -- TODO: put something more reliable in place.
-  threadDelay 1000000
+  rhpid <- forkIO $ responseHandler cout rin
+  U.logs $ "\n\n*********************forked responseHandler:rhpid=" ++ show rhpid
+  rpid <- forkIO $ reactor (ReactorState Nothing) cin cout rin
+  U.logs $ "\n\n*************************forked reactor:rpid=" ++ show rpid
 
   flip E.finally finalProc $ do
-    CTRL.run (cin,cout) hieHandlers hieOptions
+    CTRL.run rin hieHandlers hieOptions
 
   where
     handlers = [ E.Handler ioExcept
@@ -74,19 +76,60 @@ run cin = flip E.catches handlers $ do
 
 -- ---------------------------------------------------------------------
 
-responseHandler :: TChan ChannelResponse -> IO ()
-responseHandler cout = do
-  U.logm $ "\n\n****************responseHandler starting up"
+responseHandler :: TChan ChannelResponse -> TChan ReactorInput -> IO ()
+responseHandler cout cr = do
+  -- U.logm $ "\n\n************responseHandler starting up"
   forever $ do
-    r@(CResp pid rid res) <- liftIO $ atomically $ readTChan cout
-    U.logs $ "\n\n***********responseHandler: got :" ++ show r
-    -- CTRL.sendResponseMessage $ GUI.makeResponseMessage rid res
-    let smr = J.NotificationMessage "2.0" "textDocument/publishDiagnostics" (Just res)
-    U.logs $ "\n\n***********responseHandler: smr :" ++ show smr
-    CTRL.sendNotificationMessage smr
-    U.logs $ "\n\n***********responseHandler: smr sent"
+    r@(CResp pid rid res) <- atomically $ readTChan cout
+    -- U.logs $ "\n\n***********responseHandler: got :" ++ show r
+    atomically $ writeTChan cr (DispatcherResponse r)
 
-    return ()
+-- ---------------------------------------------------------------------
+
+data ReactorInput = DispatcherResponse ChannelResponse
+                  | HandlerRequest (BSL.ByteString -> IO ()) GUI.OutMessage
+
+data ReactorState =
+  ReactorState
+    { sender :: Maybe (BSL.ByteString -> IO ())
+    }
+
+reactor :: ReactorState -> TChan ChannelRequest -> TChan ChannelResponse -> TChan ReactorInput -> IO ()
+reactor s cin cout inp = do
+  inval <- liftIO $ atomically $ readTChan inp
+  s' <- case inval of
+    HandlerRequest sf (GUI.RspFromClient rm) -> do
+      U.logs $ "reactor:got RspFromClient:" ++ show rm
+      return s {sender = Just sf}
+    HandlerRequest sf (GUI.NotDidSaveTextDocument notification) -> do
+      U.logm $ "\n****** reactor: processing NotDidSaveTextDocument"
+      let J.TextDocumentIdentifier doc = J.textDocumentDidSaveTextDocumentParams $ fromJust $ J.paramsNotificationMessage notification
+          fileName = drop (length ("file://"::String)) doc
+      U.logs $ "\n********* doc=" ++ show doc
+      let req = CReq "applyrefact" 1 (IdeRequest "lint" (Map.fromList [("file", ParamFileP (T.pack fileName))])) cout
+      atomically $ writeTChan cin req
+      yield
+      return s {sender = Just sf}
+    HandlerRequest sf om -> do
+      U.logs $ "reactor:got HandlerRequest:" ++ show om
+      return s {sender = Just sf}
+    DispatcherResponse rsp@(CResp pid rid res)-> do
+      U.logs $ "reactor:got DispatcherResponse:" ++ show rsp
+      -- CTRL.sendResponseMessage $ GUI.makeResponseMessage rid res
+      let smr = J.NotificationMessage "2.0" "textDocument/publishDiagnostics" (Just res)
+      U.logs $ "\n\n***********responseHandler: smr :" ++ show smr
+      reactorSend s smr
+      U.logs $ "\n\n***********responseHandler: smr sent"
+      return s
+  reactor s' cin cout inp
+
+-- ---------------------------------------------------------------------
+
+reactorSend :: (J.ToJSON a) => ReactorState -> a -> IO ()
+reactorSend s msg =
+  case sender s of
+    Nothing -> error "reactorSend: send function not initialised yet"
+    Just sf -> sf (J.encode msg)
 
 -- ---------------------------------------------------------------------
 
@@ -96,26 +139,27 @@ hieOptions = def
 --                  , GUI.codeLensProvider = Just def
 --                  }
 
-hieHandlers :: GUI.Handlers (TChan ChannelRequest,TChan ChannelResponse)
+hieHandlers :: GUI.Handlers (TChan ReactorInput)
 hieHandlers
   = def { GUI.renameHandler   = Just renameRequestHandler
         -- , GUI.codeLensHandler = Just codeLensHandler
         , GUI.didOpenTextDocumentNotificationHandler = Just didOpenTextDocumentNotificationHandler
         , GUI.didSaveTextDocumentNotificationHandler = Just didSaveTextDocumentNotificationHandler
+        , GUI.responseHandler                        = Just responseHandlerCb
         }
 
 -- ---------------------------------------------------------------------
 
-renameRequestHandler :: GUI.Handler (TChan ChannelRequest,TChan ChannelResponse) J.RenameRequest
-renameRequestHandler cin sf (J.RequestMessage _ origId _ _) = do
+renameRequestHandler :: GUI.Handler (TChan ReactorInput) J.RenameRequest
+renameRequestHandler rin sf (J.RequestMessage _ origId _ _) = do
   let loc = def :: J.Location
       res  = GUI.makeResponseMessage origId loc
   sf (J.encode res)
 
 -- ---------------------------------------------------------------------
 
-codeLensHandler :: GUI.Handler (TChan ChannelRequest,TChan ChannelResponse) J.CodeLensRequest
-codeLensHandler (cin,cout) sf (J.RequestMessage _ origId _ _) = do
+codeLensHandler :: GUI.Handler (TChan ReactorInput) J.CodeLensRequest
+codeLensHandler rin sf (J.RequestMessage _ origId _ _) = do
   let
     lens = J.CodeLens (J.Range (J.Position 1 1) (J.Position 1 10)) (Just (J.Command "codeLensCmd" "actionCmd" Nothing)) Nothing
     lenses = [lens]
@@ -124,19 +168,27 @@ codeLensHandler (cin,cout) sf (J.RequestMessage _ origId _ _) = do
 
 -- ---------------------------------------------------------------------
 
-didOpenTextDocumentNotificationHandler :: GUI.Handler (TChan ChannelRequest,TChan ChannelResponse) J.DidOpenTextDocumentNotification
-didOpenTextDocumentNotificationHandler (cin,cout) sf notification = do
+didOpenTextDocumentNotificationHandler :: GUI.Handler (TChan ReactorInput) J.DidOpenTextDocumentNotification
+didOpenTextDocumentNotificationHandler rin sf notification = do
   U.logm "\n******** got didOpenTextDocumentNotificationHandler, ignoring"
 
 -- ---------------------------------------------------------------------
 
-didSaveTextDocumentNotificationHandler :: GUI.Handler (TChan ChannelRequest,TChan ChannelResponse)
+didSaveTextDocumentNotificationHandler :: GUI.Handler (TChan ReactorInput)
                                                       J.DidSaveTextDocumentNotification
-didSaveTextDocumentNotificationHandler (cin,cout) sf notification = do
+didSaveTextDocumentNotificationHandler rin sf notification = do
   U.logm $ "\n****** didSaveTextDocumentNotificationHandler: processing"
-  let J.TextDocumentIdentifier doc = J.textDocumentDidSaveTextDocumentParams $ fromJust $ J.paramsNotificationMessage notification
-      fileName = drop (length ("file://"::String)) doc
-  U.logs $ "\n********* doc=" ++ show doc
-  -- let req = CReq "ghcmod" 1 (IdeRequest "lint" (Map.fromList [("file", ParamFileP (T.pack fileName))])) cout
-  let req = CReq "applyrefact" 1 (IdeRequest "lint" (Map.fromList [("file", ParamFileP (T.pack fileName))])) cout
-  atomically $ writeTChan cin req
+  atomically $ writeTChan rin  (HandlerRequest sf (GUI.NotDidSaveTextDocument notification))
+  -- let J.TextDocumentIdentifier doc = J.textDocumentDidSaveTextDocumentParams $ fromJust $ J.paramsNotificationMessage notification
+  --     fileName = drop (length ("file://"::String)) doc
+  -- U.logs $ "\n********* doc=" ++ show doc
+  -- -- let req = CReq "ghcmod" 1 (IdeRequest "lint" (Map.fromList [("file", ParamFileP (T.pack fileName))])) cout
+  -- let req = CReq "applyrefact" 1 (IdeRequest "lint" (Map.fromList [("file", ParamFileP (T.pack fileName))])) cout
+  -- atomically $ writeTChan cin req
+
+-- ---------------------------------------------------------------------
+
+responseHandlerCb :: GUI.Handler (TChan ReactorInput) J.BareResponseMessage
+responseHandlerCb rin sf resp = do
+  U.logs $ "\n******** got ResponseMessage, ignoring:" ++ show resp
+
