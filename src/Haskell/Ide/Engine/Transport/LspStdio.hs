ff78d2f2b716bf049c478314fccd7172f77e7058
Alan Zimmerman <alan.zimm@gmail.com>
2017-04-27 15:55:16 +0200

    Process the balance of the diff commands
    enabling more HaRe commands

--- src/Haskell/Ide/Engine/Transport/LspStdio.hs
+++ src/Haskell/Ide/Engine/Transport/LspStdio.hs
@@ -233,7 +233,8 @@ reactor st cin cout inp = do
          }
         -}
         let
-          registration = J.Registration "lsp-demote-id" "workspace/executeCommand" Nothing
+          options = J.Object $ H.fromList [("documentSelector", J.Object $ H.fromList [("language",J.String "haskell")])]
+          registration = J.Registration "hare:demote" "workspace/executeCommand" (Just options)
         let registrations = J.RegistrationParams (J.List [registration])
         rid <- nextLspReqId
 
@@ -365,6 +366,26 @@ reactor st cin cout inp = do
                                                     cmdparams)) cout
         liftIO $ atomically $ writeTChan cin hreq
         keepOriginal rid r
+
+      -- -------------------------------
+
+      HandlerRequest sf r@(Core.ReqCompletion req) -> do
+        setSendFunc sf
+        liftIO $ U.logs $ "reactor:got CompletionRequest:" ++ show req
+        let J.TextDocumentPositionParams doc pos = fromJust $ J._params (req :: J.CompletionRequest)
+            fileName = drop (length ("file://"::String)) $ J._uri (doc :: J.TextDocumentIdentifier)
+            J.Position l c = pos
+        -- rid <- nextReqId
+        -- let hreq = CReq "ghcmod" rid (IdeRequest "type" (Map.fromList
+        --                                             [("file",     ParamFileP (T.pack fileName))
+        --                                             ,("start_pos",ParamValP $ ParamPos (toPos (l+1,c+1)))
+        --                                             ])) cout
+        -- liftIO $ atomically $ writeTChan cin hreq
+        -- keepOriginal rid r
+
+        let cr = J.Completions [] -- ( [] :: [J.CompletionListType])
+        let rspMsg = Core.makeResponseMessage (J.responseId $ J._id (req :: J.CompletionRequest)) cr
+        reactorSend rspMsg
 
       -- -------------------------------
 
@@ -543,17 +564,9 @@ posToPosition (Pos (Line l) (Col c)) = J.Position (l-1) (c-1)
 -- ---------------------------------------------------------------------
 
 hieOptions :: Core.Options
--- hieOptions = def
--- hieOptions = def { Core.executeCommandProvider = Just (J.ExecuteCommandOptions (J.List ["hie-command"]))
---                  }
--- hieOptions = def { Core.textDocumentSync = Just J.TdSyncNone
---                  , Core.executeCommandProvider = Just (J.ExecuteCommandOptions (J.List ["lsp-demote-id"]))
---                  }
--- hieOptions = def { Core.textDocumentSync = Just J.TdSyncFull
---                  , Core.executeCommandProvider = Just (J.ExecuteCommandOptions (J.List ["applyrefact:applyOne"]))
---                  }
 hieOptions = def { Core.textDocumentSync = Just J.TdSyncIncremental
-                 , Core.executeCommandProvider = Just (J.ExecuteCommandOptions (J.List ["applyrefact:applyOne"]))
+                 , Core.completionProvider = Just (J.CompletionOptions (Just True) Nothing)
+                 , Core.executeCommandProvider = Just (J.ExecuteCommandOptions (J.List ["applyrefact:applyOne","hare:demote"]))
                  }
 
 
@@ -570,6 +583,8 @@ hieHandlers rin
         , Core.responseHandler                          = Just $ responseHandlerCb rin
         , Core.codeActionHandler                        = Just $ passHandler rin Core.ReqCodeAction
         , Core.executeCommandHandler                    = Just $ passHandler rin Core.ReqExecuteCommand
+        , Core.completionHandler                        = Just $ passHandler rin Core.ReqCompletion
+        , Core.completionResolveHandler                 = Just $ passHandler rin Core.ReqCompletionItemResolve
         }
 
 -- ---------------------------------------------------------------------
@@ -594,31 +609,41 @@ refactorResultToWorkspaceEdit (RefactorResult diffs) = J.WorkspaceEdit (Just r) 
 
 -- TODO: perhaps move this somewhere else, for general use
 hieDiffToLspEdit :: HieDiff -> (T.Text, J.List (J.TextEdit))
-hieDiffToLspEdit (HieDiff f _ d) = (T.pack ("file://" ++ f),J.List r)
+hieDiffToLspEdit (HieDiff f _ d) = (T.pack ("file://" ++ f), J.List r)
   where
     pd = parsePrettyDiffs d
-    -- r = error $ "hieDiffToLspEdit:pd=" ++ show pd
     r = map diffOperationToTextEdit pd
-{-
-hie: hieDiffToLspEdit:pd=
- [Change (LineRange {lrNumbers = (8,8), lrContents = ["baz = do"]})
-         (LineRange {lrNumbers = (8,8), lrContents = ["baz ="]})]
--}
 
-diffOperationToTextEdit :: DiffOperation LineRange -> J.TextEdit
-diffOperationToTextEdit (Change fm to) = J.TextEdit r nt
-  where
-    sl = fst $ lrNumbers fm
-    sc = 0
-    s = J.Position (sl - 1) sc -- Note: zero-based lines
-    el = snd $ lrNumbers fm
-    ec = length $ last $ lrContents fm
-    e = J.Position (el - 1) ec  -- Note: zero-based lines
-    r = J.Range s e
-    nt = intercalate "\n" $ lrContents to
+    {-
+    hie: hieDiffToLspEdit:pd=
+     [Change (LineRange {lrNumbers = (8,8), lrContents = ["baz = do"]})
+             (LineRange {lrNumbers = (8,8), lrContents = ["baz ="]})]
+    -}
 
--- diffOperationToTextEdit (Deletion fm _) = J.TextEdit r ""
---   where
+    diffOperationToTextEdit :: DiffOperation LineRange -> J.TextEdit
+    diffOperationToTextEdit (Change fm to) = J.TextEdit range nt
+      where
+        range = calcRange fm
+        nt = init $ unlines $ lrContents to
+
+    diffOperationToTextEdit (Deletion fm _) = J.TextEdit range ""
+      where
+        range = calcRange fm
+
+    diffOperationToTextEdit (Addition fm _) = J.TextEdit range nt
+      where
+        range = calcRange fm
+        nt = unlines $ lrContents fm
+
+
+    calcRange fm = J.Range s e
+      where
+        sl = fst $ lrNumbers fm
+        sc = 0
+        s = J.Position (sl - 1) sc -- Note: zero-based lines
+        el = snd $ lrNumbers fm
+        ec = length $ last $ lrContents fm
+        e = J.Position (el - 1) ec  -- Note: zero-based lines
 {-
 
 Turn
@@ -681,6 +706,7 @@ diagnostic = do
   severity <- optionSeverity
   msglines <- sepEndBy (many1 (noneOf "\n\0")) (char '\0')
   let pos = (Position (l-1) (c-1))
+  -- AZ:TODO: consider setting pprCols dflag value in the call, for better format on vscode
   return (fname,[Diagnostic (Range pos pos) (Just severity) Nothing (Just "ghcmod") (unlines msglines)] )
 
 optionSeverity :: P DiagnosticSeverity
