92c15f405492fbb3f4bcf554e8136d75b3081fa2
Alan Zimmerman <alan.zimm@gmail.com>
2017-05-03 17:36:51 +0200

    Build hoogle db if not present, and make pedantic clean

--- test/HooglePluginSpec.hs
+++ test/HooglePluginSpec.hs
@@ -2,21 +2,19 @@
 module HooglePluginSpec where
 
 import           Control.Concurrent.STM.TChan
+import           Control.Monad
 import           Control.Monad.STM
-import           Control.Exception
 import           Data.Aeson
 import qualified Data.HashMap.Strict as H
-import qualified Data.Text as T
 import           Haskell.Ide.Engine.Dispatcher
 import           Haskell.Ide.Engine.Monad
-import           Haskell.Ide.Engine.MonadFunctions
 import           Haskell.Ide.Engine.PluginDescriptor
-import           Haskell.Ide.Engine.SemanticTypes
 import           Haskell.Ide.Engine.Types
 import           Haskell.Ide.HooglePlugin
 import           System.Directory
 import qualified Data.Map as Map
 import           TestUtils
+import           Hoogle
 
 import           Test.Hspec
 
@@ -45,6 +43,11 @@ dispatchRequest req = do
 
 hoogleSpec :: Spec
 hoogleSpec = do
+  describe "hoogle environment" $ do
+    it "Checks the default dababase location" $ do
+      db <- defaultDatabaseLocation
+      exists <- doesFileExist db
+      unless exists $ do hoogle ["generate"]
   describe "hoogle plugin commands" $ do
     it "runs the info command" $ do
       let req = IdeRequest "info" (Map.fromList [("expr", ParamTextP "head")])
@@ -56,6 +59,16 @@ hoogleSpec = do
     it "runs the lookup command" $ do
       let req = IdeRequest "lookup" (Map.fromList [("term", ParamTextP "[a] -> a")])
       r <- dispatchRequest req
-      r `shouldBe` Just (IdeResponseOk (H.fromList ["ok" .= ["Prelude head :: [a] -> a","Prelude last :: [a] -> a","Data.List head :: [a] -> a", "Data.List last :: [a] -> a", "GHC.OldList head :: [a] -> a", "GHC.OldList last :: [a] -> a", "Distribution.Compat.Semigroup mconcat :: [a] -> a", "System.Console.CmdArgs.Quote modes# :: [a] -> a", "System.Console.CmdArgs.Quote enum# :: [a] -> a", "CorePrelude mconcat :: [a] -> a" :: String] ]  ))
+      r `shouldBe` Just (IdeResponseOk (H.fromList ["ok" .=
+                          [ "Prelude head :: [a] -> a","Prelude last :: [a] -> a"
+                          , "Data.List head :: [a] -> a"
+                          , "Data.List last :: [a] -> a"
+                          , "GHC.OldList head :: [a] -> a"
+                          , "GHC.OldList last :: [a] -> a"
+                          , "Distribution.Compat.Semigroup mconcat :: [a] -> a"
+                          , "System.Console.CmdArgs.Quote modes# :: [a] -> a"
+                          , "System.Console.CmdArgs.Quote enum# :: [a] -> a"
+                          , "CorePrelude mconcat :: [a] -> a"
+                            :: String] ]  ))
 
 
