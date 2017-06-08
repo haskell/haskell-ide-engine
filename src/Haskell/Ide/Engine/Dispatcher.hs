18b33eebae4949b4ac7b95b790fe6bd8fb7833f9
Zubin Duggal <zubin.duggal@gmail.com>
2017-06-08 17:38:01 +0530

    Added dispatcher for new plugin transport

--- src/Haskell/Ide/Engine/Dispatcher.hs
+++ src/Haskell/Ide/Engine/Dispatcher.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE GADTs #-}
 module Haskell.Ide.Engine.Dispatcher where
 
@@ -35,6 +36,16 @@ dispatcher cin = do
     case mresp of
       Nothing -> return ()
       Just resp -> liftIO $ sendResponse req resp
+
+dispatcherP :: TChan PluginRequest -> IdeM ()
+dispatcherP pin = forever $ do
+  debugm "dispatcherP: top of loop"
+  (PReq plugin reqId replyChan action) <- liftIO $ atomically $ readTChan pin
+  debugm $ "main loop:got:" ++ show (plugin, reqId)
+  resp <- fmap wrapResponse <$> action
+  let reply = PResp plugin reqId resp
+  debugm $ "sending response: " ++ show reply
+  liftIO $ atomically $ writeTChan replyChan reply
 
 -- ---------------------------------------------------------------------
 
