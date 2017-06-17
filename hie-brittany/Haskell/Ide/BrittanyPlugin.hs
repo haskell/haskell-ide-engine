e70fac04a1e7f3bbcb312510674f02d8e01c8ea0
Zubin Duggal <zubin.duggal@gmail.com>
2017-06-17 15:05:41 +0530

    Added support for textDocument/formatting request using brittany

---
+++ hie-brittany/Haskell/Ide/BrittanyPlugin.hs
@@ -0,0 +1,75 @@
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+
+{-# LANGUAGE DataKinds             #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE GADTs                 #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+{-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE LambdaCase            #-}
+module Haskell.Ide.BrittanyPlugin where
+
+import           Data.Aeson
+import           Data.Monoid
+import qualified Data.Text as T
+import           Data.Vinyl
+import           Haskell.Ide.Engine.PluginDescriptor
+import           Haskell.Ide.Engine.PluginUtils
+import           Control.Monad.IO.Class
+import           Language.Haskell.Brittany
+import           Language.Haskell.Brittany.Types
+import           Language.Haskell.Brittany.Config
+import qualified DynFlags as GHC
+import qualified GHC.LanguageExtensions.Type as GHC
+import qualified Data.Text.Lazy as TextL
+import qualified GhcMod.Utils as GM
+import qualified Language.Haskell.LSP.TH.DataTypesJSON as J
+import           Control.Lens
+
+
+brittanyCmd :: TextDocumentIdentifier -> Maybe Range -> IdeM (IdeResponse [J.TextEdit])
+brittanyCmd tdi range =
+  pluginGetFile "genapplicative: " (tdi ^. J.uri) $ \file -> do
+    case range of
+      Just r -> error "not handled yet"
+      Nothing -> do
+        text <- GM.withMappedFile file $ liftIO . readFile
+        res <- liftIO $ runBrittany text
+        case res of
+          Left err -> return $ IdeResponseFail (IdeError PluginError
+                      (T.pack $ "brittanyCmd: " ++ show err) Null)
+          Right newText -> do
+            let newString = TextL.unpack newText
+                startPos = Position 0 0
+                endPos = toPos (l,c+1)
+                l = length $ textLines
+                c = length $ last textLines
+                textLines = lines text
+                textEdit = J.TextEdit (Range startPos endPos) $ T.pack newString
+            return $ IdeResponseOk [textEdit]
+
+runBrittany :: String -> IO (Either String TextL.Text)
+runBrittany text = do
+    let config = staticDefaultConfig
+    let ghcOptions = []
+    let cppCheckFunc dynFlags = if GHC.xopt GHC.Cpp dynFlags
+          then return $ Left "Encountered -XCPP. Aborting."
+          else return $ Right False
+    parseResult <- parseModuleFromString ghcOptions "stdin" cppCheckFunc text
+    case parseResult of
+      Left left -> do
+        return $ Left left
+      Right (anns, parsedSource, _) -> do
+        (errsWarns, outLText) <- pPrintModuleAndCheck config anns parsedSource
+        let shouldOutput = null errsWarns
+        if shouldOutput then
+          return $ Right outLText
+        else
+          return $ Left $ unlines $ map showErrs errsWarns
+
+showErrs :: LayoutError -> String
+showErrs (LayoutErrorUnusedComment s) = s
+showErrs (LayoutWarning s) = s
+showErrs (LayoutErrorUnknownNode s _) = s
+showErrs LayoutErrorOutputCheck = "Brittany error - invalid output"
+
