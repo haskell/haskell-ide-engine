65697c2274b27ba61a517d9734d797e9068b7281
Zubin Duggal <zubin.duggal@gmail.com>
2017-07-25 23:11:18 +0530

    Add LINE pragma to mapped files to improve error reporting

--- src/Haskell/Ide/Engine/Transport/LspStdio.hs
+++ src/Haskell/Ide/Engine/Transport/LspStdio.hs
@@ -177,7 +177,8 @@ mapFileFromVfs verTVar cin vtdi = do
   mvf <- liftIO $ vfsFunc uri
   case (mvf, uriToFilePath uri) of
     (Just (VFS.VirtualFile _ yitext), Just fp) -> do
-      let text = Yi.toString yitext
+      let text' = Yi.toString yitext
+          text = "{-# LINE 1 \"" ++ fp ++ "\"#-}\n" <> text'
       let req = PReq (Just uri) Nothing Nothing (const $ return ())
                   $ IdeResponseOk <$> GM.loadMappedFileSource fp text
       liftIO $ atomically $ do
