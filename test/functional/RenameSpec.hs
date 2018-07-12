3a8a5a8e04c03d35c5cc783fe0991fbaec73d732
Luke Lau <luke_lau@icloud.com>
2018-07-12 15:43:13 +0100

    Add rename tests

---
+++ test/functional/RenameSpec.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+module RenameSpec where
+
+import Control.Monad.IO.Class
+import Language.Haskell.LSP.Test
+import Language.Haskell.LSP.Types
+import Test.Hspec
+import TestUtils
+
+spec :: Spec
+spec = describe "rename" $
+  it "works" $ runSession hieCommand "test/testdata" $ do
+    doc <- openDoc "Rename.hs" "haskell"
+    rename doc (Position 3 1) "baz" -- foo :: Int -> Int
+    documentContents doc >>= liftIO . flip shouldBe expected
+  where
+    expected =
+      "main = do\n\
+      \  x <- return $ baz 42\n\
+      \  return (baz x)\n\
+      \baz :: Int -> Int\n\
+      \baz x = x + 1\n\
+      \bar = (+ 1) . baz\n"
