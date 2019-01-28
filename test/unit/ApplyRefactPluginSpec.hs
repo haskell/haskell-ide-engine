827359617869774b31684136101d103da392ae2c
Alan Zimmerman <alan.zimm@gmail.com>
2019-01-28 23:12:26 +0200

    Fix hlint test for GHC 8.2.1, using older hlint

--- test/unit/ApplyRefactPluginSpec.hs
+++ test/unit/ApplyRefactPluginSpec.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE OverloadedStrings     #-}
 module ApplyRefactPluginSpec where
@@ -97,6 +98,7 @@ applyRefactSpec = do
             PublishDiagnosticsParams
              { _uri = filePath
              , _diagnostics = List
+#if (defined(MIN_VERSION_GLASGOW_HASKELL) && (MIN_VERSION_GLASGOW_HASKELL(8,2,2,0)))
                [Diagnostic {_range = Range { _start = Position {_line = 13, _character = 0}
                                            , _end = Position {_line = 13, _character = 100000}}
                            , _severity = Just DsInfo
@@ -104,6 +106,15 @@ applyRefactSpec = do
                            , _source = Just "hlint"
                            , _message = "Parse error: virtual }\n  data instance Sing (z :: (a :~: b)) where\n      SRefl :: Sing Refl +\n> \n\n"
                            , _relatedInformation = Nothing }]}
+#else
+               [Diagnostic {_range = Range { _start = Position {_line = 11, _character = 28}
+                                           , _end = Position {_line = 11, _character = 100000}}
+                           , _severity = Just DsInfo
+                           , _code = Just "parser"
+                           , _source = Just "hlint"
+                           , _message = "Parse error: :~:\n  import           Data.Type.Equality            ((:~:) (..), (:~~:) (..))\n  \n> data instance Sing (z :: (a :~: b)) where\n      SRefl :: Sing Refl +\n\n"
+                           , _relatedInformation = Nothing }]}
+#endif
       testCommand testPlugins act "applyrefact" "lint" arg res
 
     -- ---------------------------------
