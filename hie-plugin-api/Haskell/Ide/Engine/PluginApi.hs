d71de15df57439a006cd394357c85db42e901071
Alan Zimmerman <alan.zimm@gmail.com>
2019-04-14 15:43:46 +0200

    Expose ghc-mod features currently used in HaRe tests

--- hie-plugin-api/Haskell/Ide/Engine/PluginApi.hs
+++ hie-plugin-api/Haskell/Ide/Engine/PluginApi.hs
@@ -27,12 +27,18 @@ module Haskell.Ide.Engine.PluginApi
   , GM.GmEnv(..)
   , GM.gmlGetSession
   , GM.gmlSetSession
+  , GM.globalArgSpec
+  , GM.OutputOpts(..)
+  , GM.GmLogLevel(..)
+  , GM.OutputStyle(..)
+  , GM.LineSeparator(..)
   ) where
 
 import qualified GhcMod.Monad.Newtypes as GM (GmlT(..))
 import qualified GhcMod.Monad.Out      as GM (GmOut(..))
 import qualified GhcMod.Monad.Types    as GM (GmEnv(..),IOish,gmlGetSession,gmlSetSession,cradle)
 import qualified GhcMod.Target         as GM (cabalResolvedComponents)
-import qualified GhcMod.Types          as GM (ModulePath(..),GmModuleGraph(..),GmComponent(..),GmComponentType(..),Cradle(..),MonadIO(..))
+import qualified GhcMod.Types          as GM (ModulePath(..),GmModuleGraph(..),GmComponent(..),GmComponentType(..),Cradle(..),MonadIO(..),OutputOpts(..),GmLogLevel(..),OutputStyle(..),LineSeparator(..))
 import qualified GhcMod.Utils          as GM (mkRevRedirMapFunc)
 import qualified GhcModCore            as GM (Options(..),defaultOptions,getModulesGhc',GhcModT,runGhcModT)
+import qualified GhcMod.Options.Options as GM (globalArgSpec)
