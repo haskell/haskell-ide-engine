7132502a80a8295fbb0007f7de80345f94a11ad7
Daniel Gröber <dxld@darkboxed.org>
2019-07-27 14:26:31 +0200

    tests: Run hie command directly, not through stack exec
    There seems to be no need for this, not sure if there ever was. Both stack
    and cabal setup the environment so we can find `hie` on PATH.

--- test/utils/TestUtils.hs
+++ test/utils/TestUtils.hs
@@ -163,13 +163,12 @@ logFilePath :: String
 logFilePath = "functional-hie-" ++ stackYaml ++ ".log"
 
 -- | The command to execute the version of hie for the current compiler.
--- Make sure to disable the STACK_EXE and GHC_PACKAGE_PATH environment
--- variables or else it messes up -- ghc-mod.
--- We also need to unset STACK_EXE manually inside the tests if they are
--- run with `stack test`
+--
+-- Both @stack test@ and @cabal new-test@ setup the environment so @hie@ is
+-- on PATH. Cabal seems to respond to @build-tool-depends@ specifically while
+-- stack just puts all project executables on PATH.
 hieCommand :: String
-hieCommand = "stack exec --no-stack-exe --no-ghc-package-path --stack-yaml=" ++ stackYaml ++
-             " hie -- -d -l test-logs/" ++ logFilePath
+hieCommand = "hie -d -l test-logs/" ++ logFilePath
 
 hieCommandVomit :: String
 hieCommandVomit = hieCommand ++ " --vomit"
