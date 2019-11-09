d646605fb3e2de7960265d56257ab8b9e57706b7
Alan Zimmerman <alan.zimm@gmail.com>
2019-11-09 14:58:50 +0000

    Make sure test dispatcher runs in a clean environment

--- test/dispatcher/Main.hs
+++ test/dispatcher/Main.hs
@@ -21,6 +21,7 @@ import           Haskell.Ide.Engine.Types
 import           Language.Haskell.LSP.Types
 import           TestUtils
 import           System.Directory
+import           System.Environment
 import           System.FilePath
 
 import           Test.Hspec
@@ -71,12 +72,19 @@ startServer :: IO (Scheduler IO, TChan LogVal, ThreadId)
 startServer = do
   scheduler <- newScheduler plugins testOptions
   logChan  <- newTChanIO
-  dispatcher <- forkIO $
+  dispatcher <- forkIO $ do
+    -- We need to clear these environment variables to prevent
+    -- collisions with stack usages
+    -- See https://github.com/commercialhaskell/stack/issues/4875
+    unsetEnv "GHC_PACKAGE_PATH"
+    unsetEnv "GHC_ENVIRONMENT"
+    unsetEnv "HASKELL_PACKAGE_SANDBOX"
+    unsetEnv "HASKELL_PACKAGE_SANDBOXES"
     runScheduler
-    scheduler
-    (\lid errCode e -> logToChan logChan ("received an error", Left (lid, errCode, e)))
-    (\g x -> g x)
-    def
+      scheduler
+      (\lid errCode e -> logToChan logChan ("received an error", Left (lid, errCode, e)))
+      (\g x -> g x)
+      def
 
   return (scheduler, logChan, dispatcher)
 
