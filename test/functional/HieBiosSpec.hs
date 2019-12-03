fea727172f3f1bf4ed17e57f5ce6544dec7f499c
jneira <atreyu.bbb@gmail.com>
2019-12-03 06:31:14 +0100

    Create empty hie.yaml to trigger a parse error

--- test/functional/HieBiosSpec.hs
+++ test/functional/HieBiosSpec.hs
@@ -6,19 +6,29 @@ import qualified Data.Text as T
 import Language.Haskell.LSP.Test
 import Language.Haskell.LSP.Types
 import Language.Haskell.LSP.Messages
+import System.FilePath ((</>))
 import Test.Hspec
 import TestUtils
 
 spec :: Spec
-spec = describe "hie-bios" $ do
-  it "loads modules inside main-is" $ runSession hieCommand fullCaps "test/testdata/hieBiosMainIs" $ do
-    _ <- openDoc "Main.hs" "haskell"
-    _ <- count 2 waitForDiagnostics
-    return ()
-  it "reports errors in hie.yaml" $ runSession hieCommand fullCaps "test/testdata/hieBiosError" $ do
-    _ <- openDoc "Foo.hs" "haskell"
-    _ <- skipManyTill loggingNotification (satisfy isMessage)
-    return ()
-  where isMessage (NotShowMessage (NotificationMessage _ _ (ShowMessageParams MtError s))) =
+-- Create an empty hie.yaml to trigger the parse error
+spec = beforeAll_ (writeFile (hieBiosErrorPath </> "hie.yaml") "") $ do
+
+  describe "hie-bios" $ do
+
+    it "loads modules inside main-is" $ runSession hieCommand fullCaps "test/testdata/hieBiosMainIs" $ do
+      _ <- openDoc "Main.hs" "haskell"
+      _ <- count 2 waitForDiagnostics
+      return ()
+  
+    it "reports errors in hie.yaml" $ runSession hieCommand fullCaps hieBiosErrorPath $ do
+      _ <- openDoc "Foo.hs" "haskell"
+      _ <- skipManyTill loggingNotification (satisfy isMessage)
+      return ()
+  
+  where hieBiosErrorPath = "test/testdata/hieBiosError"
+  
+        isMessage (NotShowMessage (NotificationMessage _ _ (ShowMessageParams MtError s))) =
           "Couldn't parse hie.yaml" `T.isInfixOf` s
         isMessage _ = False
+        
