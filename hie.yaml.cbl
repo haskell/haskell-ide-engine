5c6788f2f4e7a828a0172704f07c643646750f7a
Alan Zimmerman <alan.zimm@gmail.com>
2020-01-01 12:04:40 +0000

    Add some documentation

--- hie.yaml.cbl
+++ hie.yaml.cbl
@@ -1,3 +1,7 @@
+# This is a sample hie.yaml file for opening haskell-ide-engine in
+# hie, using cabal as the build system.
+# To use is, copy it to a file called 'hie.yaml'
+
 cradle:
   cabal:
     - path: "./hie-plugin-api/Haskell"
