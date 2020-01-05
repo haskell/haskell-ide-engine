8afaba0ffedd0b81da71e32a4207348f48ce88cc
Luke Lau <luke_lau@icloud.com>
2020-01-05 14:50:10 +0000

    Force module cache test case to use cabal cradle

--- test/utils/TestUtils.hs
+++ test/utils/TestUtils.hs
@@ -153,7 +153,6 @@ files =
    , "./test/testdata/gototest/"
    , "./test/testdata/redundantImportTest/"
    , "./test/testdata/wErrorTest/"
-   , "./test/testdata/multiComponent/"
   ]
 
 data GhcVersion
