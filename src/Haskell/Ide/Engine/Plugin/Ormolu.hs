451e66dec83d8a5f46d5523ac53f1b43e966f936
Avi Dessauer <avi.the.coder@gmail.com>
2020-01-21 00:56:56 -0500

    Pass cradle wide extensions to ormolu

--- src/Haskell/Ide/Engine/Plugin/Ormolu.hs
+++ src/Haskell/Ide/Engine/Plugin/Ormolu.hs
@@ -10,11 +10,13 @@ import Haskell.Ide.Engine.MonadTypes
 import Control.Exception
 import Control.Monad.IO.Class ( liftIO , MonadIO(..) )
 import Data.Aeson ( Value ( Null ) )
-import Data.Text
+import Data.List
+import qualified Data.Text as T
 import Ormolu
-import Ormolu.Config (defaultConfig)
 import Ormolu.Exception (OrmoluException)
 import Haskell.Ide.Engine.PluginUtils
+import HIE.Bios.Flags
+import HIE.Bios.Types
 #endif
 
 ormoluDescriptor :: PluginId -> PluginDescriptor
@@ -32,15 +34,26 @@ ormoluDescriptor plId = PluginDescriptor
 
 
 provider :: FormattingProvider
-provider _contents _uri _ _typ _opts =
+provider _contents _uri _mcradle _typ _opts =
 #if __GLASGOW_HASKELL__ >= 806
-  case _typ of 
-    FormatRange _ -> return $ IdeResultFail (IdeError PluginError (pack "Selection formatting for Ormolu is not currently supported.") Null)
+  case _typ of
+    FormatRange _ -> return $ IdeResultFail (IdeError PluginError (T.pack "Selection formatting for Ormolu is not currently supported.") Null)
     FormatText -> pluginGetFile _contents _uri $ \file -> do
-        result <- liftIO $ try @OrmoluException (ormolu defaultConfig file (unpack _contents))
+        result <- do
+          res <- liftIO $ sequenceA $ getCompilerOptions file <$> _mcradle
+          let opts = case res of
+                      Just (CradleSuccess o) -> fmap DynOption $ filter exop $ componentOptions o
+                      _ -> []
+              conf = Config opts False False True False
+          liftIO $ try @OrmoluException (ormolu conf file (T.unpack _contents))
         case result of
-          Left  err -> return $ IdeResultFail (IdeError PluginError (pack $  "ormoluCmd: " ++ show err) Null)
+          Left  err -> return $ IdeResultFail (IdeError PluginError (T.pack $  "ormoluCmd: " ++ show err) Null)
           Right new -> return $ IdeResultOk [TextEdit (fullRange _contents) new]
+  where
+    exop s =
+      "-X" `isPrefixOf` s
+      || "-fplugin=" `isPrefixOf` s
+      || "-pgmF=" `isPrefixOf` s
 #else
   return $ IdeResultOk [] -- NOP formatter
 #endif
